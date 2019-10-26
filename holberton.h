17a0de811a6d49da6f43533fe1e55391f27d00b7
Gaspela04 <samirmillanorozco@hotmail.com>
2019-10-26 16:39:13 +0000

    Printf

---
+++ holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
+
+int _printf(const char *format, ...);
+
+#endif /* HOLBERTON_H */
